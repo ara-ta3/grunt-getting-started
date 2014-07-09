38ff8bcbadef27a4475e000da80c9cf653c598c4
Arata <tarata43@yahoo.co.jp>
2014-07-09 23:31:59 +0900

    csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,27 @@
 module.exports = function(grunt) {
 
-    /*
-      task: {
-        target1: {
-            src: ....,
-            dest: ....
-            }
-        }
-      task: {
-        target1: {
-            files: {
-                dest:src
-            }
-        }
-
-     */
-
     // config
     grunt.initConfig({
 
         less:{
-             build1: {
-            /*
-                src : 'src/style1.less',
-                dest: 'build/styles1.css'
-                */
-                files:{
-//                          'build/styles1.css':'src/style1.less'
-//                          'build/styles1.css':'src/*.less'
-                          'build/styles1.css':'src/**/*.less'
-                      },
-                options:{
-                    compress:true
-                }
-             },
-             build2: {
-                files:{
-                          'build/styles1.css':'src/**/*.less'
-                  }
+             build: {
+                src : ['src/style1.less','src/style2.less'],
+                dest: 'build/styles.css'
              }
-
-        }
+        },
+        csslint:{
+            check: {
+                //src: 'build/styles.css'
+               src: '<%= less.build.dest %>'
+            }
+        },
 
     });
 
     // plugin
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 
     // tasks
-    grunt.registerTask('default', 'less');
-    grunt.registerTask('task1', 'less:build1');
-    grunt.registerTask('task2', 'less:build2');
+    grunt.registerTask('default', ['less', 'csslint']);
 };
